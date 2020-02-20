62cdd730681cfa0d93a15338fb142a991fe96294
Brennan Roel <brennanroel@gmail.com>
2020-02-20 01:28:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,8 @@ end
 
 roll_call_dwarves(dwarves)
 
+
+
 def summon_captain_planet(array)
   nu_array = []
   i = 0 
@@ -23,3 +25,29 @@ def summon_captain_planet(array)
 end
 
 summon_captain_planet(planeteer_calls)
+
+
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
+end
+
+long_planeteer_calls(call_screams)
+
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+
+def find_the_cheese2(array, array2)
+  cheese_types = array2
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
+
+find_the_cheese2(snacks, cheese_types)
